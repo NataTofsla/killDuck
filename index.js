5a4cc95605ce5494ad740a3beb9f0c50441e35d9
Natalya Putintseva <n.putintseva@ftc.ru>
2019-02-04 10:24:22 +0700

    Полёт уток сделан с помощью 4 функций

--- index.js
+++ index.js
@@ -3,42 +3,70 @@ document.addEventListener('DOMContentLoaded', function (ev) {
 	var dog = document.querySelector('.dog');
 	var duck = document.querySelector('.duck');
 	var skyWidth = document.documentElement.clientWidth - 105;
-	var skyHeight = document.documentElement.clientHeight - 220;
+	var skyHeight = document.documentElement.clientHeight - 226;
 
 	var dogJump = function dogJump() {
 		dog.classList.remove('dog_walk');
 		dog.classList.add('dog_jump');
 	};
 
+	function duckRight() {
+		duck.classList.add('duck_fly');		
+		duck.classList.add('duck_fly_right');
+		duck.style.left = skyWidth + 'px';
+	};
+
+	function duckLeft() {
+		duck.classList.add('duck_fly');					
+		duck.classList.add('duck_fly_left');
+		duck.style.left = '5px';
+	};
+
+		function duckRightTop() {
+		duck.classList.add('duck_fly');				
+		duck.classList.add('duck_fly_top_right');
+		duck.style.left = skyWidth + 'px';
+		duck.style.top = '15px';		
+	};
+
+		function duckLeftTop() {
+		duck.classList.add('duck_fly');				
+		duck.classList.add('duck_fly_top_left');
+		duck.style.left = '5px';
+		duck.style.top = '15px';
+	};
+
+
 	function selectDuck() {
 		duck.classList.remove('duck_fly_left', 'duck_fly_top_left', 'duck_fly_right', 'duck_fly_top_right');
+	
+		var rand2 = Math.round(0.5 + Math.random() * 4);
+		
+		switch (rand2) {
+			case 1:
+				duckRight();
+				break;
+			case 2:
+				duckLeft();
+				break;
+			case 3:
+				duckRightTop();
+				break;
+			default:
+				duckLeftTop();
+				break;
+		};
 
-        var positionX = duck.offsetTop;
-        var positionY = duck.offsetLeft;			
-        var x = Math.floor(Math.random()*(skyWidth));
-        var y = Math.floor(Math.random()*(skyHeight));
 
-
-        if ((positionX < x) && (positionY < y)) {
-        		duck.classList.add('duck_fly_top_right');
-			} else if ((positionX < x) && (positionY > y)) {
-				duck.classList.add('duck_fly_right');
-			} else if ((positionX > x) && (positionY < y)){
-				duck.classList.add('duck_fly_top_left');
-        	} else {
-        		duck.classList.add('duck_fly_left');
-        };
-     
-		duck.classList.add('duck_fly');
-	    duck.style.left = x + 'px';
-		duck.style.top = y + 'px';       
 
     };
 
 	var moveDuck = function moveDuck() {
 	    setInterval(function() {
-	    	selectDuck();    
+			duck.style.top = skyHeight + 'px';		    	
+	    	selectDuck();   
 	    },3000);
+
 	};
 
 	var startGame = function startGame() {
